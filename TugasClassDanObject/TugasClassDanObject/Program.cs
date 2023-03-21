ea80ee4fc122d0ef18325a84c28af301d4e176be
Naufal4965 <127710312+Naufal4965@users.noreply.github.com>
2023-03-22 00:39:17 +0700

    Add files via upload

---
+++ TugasClassDanObject/TugasClassDanObject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "Jamal";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+            //mudah-mudahan selesai
+        }
+    }
+}
---
+++ TugasClassDanObject/TugasClassDanObject/Taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("\n Taxi Info \n");
+            Console.WriteLine("Nama Driver: {0}", DriverName);
+
+            if(OnDuty==true)
+            {
+                Console.WriteLine("Kerja : Iya", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine("Kerja : Tidak", OnDuty);
+            }
+            Console.WriteLine("Nomor Pelanggan : {0}", NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+}
